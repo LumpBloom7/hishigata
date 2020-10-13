92adbb88ca49259f35971dfbf297c501d0d3fbfe
Derrick Timmermans <derrick.timmermans@outlook.com>
2020-10-13 19:46:56 +0200

    Add HitExplosions

---
+++ osu.Game.Rulesets.Hishigata/UI/HitExplosion.cs
@@ -0,0 +1,55 @@
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Game.Graphics;
+using osu.Game.Rulesets.Objects.Drawables;
+using osu.Game.Rulesets.Hishigata.Objects.Drawables;
+using osu.Game.Rulesets.Scoring;
+using osuTK.Graphics;
+using osuTK;
+
+namespace osu.Game.Rulesets.Hishigata.UI
+{
+    internal class HitExplosion : CompositeDrawable
+    {
+        private readonly Box left;
+        private readonly Box right;
+        private readonly double animationTime;
+
+        public HitExplosion(DrawableHishigataHitObject h)
+        {
+            animationTime = h.HitObject.TimePreempt / 3;
+            Anchor = Anchor.Centre;
+            Origin = Anchor.Centre;
+            Position = new Vector2(0, -80);
+            InternalChildren = new Drawable[]
+            {
+                left = new Box
+                {
+                    Origin = Anchor.CentreRight,
+                    Anchor = Anchor.Centre,
+                    Size = new Vector2(0,4),
+                    Rotation = -45
+                },
+                right = new Box
+                {
+                    Origin = Anchor.CentreLeft,
+                    Anchor = Anchor.Centre,
+                    Size = new Vector2(0,4),
+                    Rotation = 45
+                },
+            };
+
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            left.ResizeWidthTo(100, animationTime).ResizeHeightTo(0, animationTime);
+            right.ResizeWidthTo(100, animationTime).ResizeHeightTo(0, animationTime);
+            this.Delay(animationTime).Expire();
+        }
+    }
+}
--- osu.Game.Rulesets.Hishigata/UI/Lane.cs
+++ osu.Game.Rulesets.Hishigata/UI/Lane.cs
@@ -2,21 +2,38 @@ using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Rulesets.UI;
+using osu.Game.Rulesets.Judgements;
 using static osu.Game.Input.Handlers.ReplayInputHandler;
 using osuTK;
 using System.Linq;
 using osu.Framework.Input.Bindings;
 using osu.Framework.Allocation;
 using osu.Game.Rulesets.UI.Scrolling;
+using osu.Game.Rulesets.Objects.Drawables;
+using osu.Game.Rulesets.Hishigata.Objects.Drawables;
 
 namespace osu.Game.Rulesets.Hishigata.UI
 {
     public class Lane : Playfield
     {
+        private readonly Container hitExplosionContainer;
         public Lane()
         {
             Anchor = Anchor.Centre;
             Origin = Anchor.Centre;
+            AddInternal(hitExplosionContainer = new Container());
+        }
+
+        public override void Add(DrawableHitObject h)
+        {
+            h.OnNewResult += onNewResult;
+            base.Add(h);
+        }
+
+        private void onNewResult(DrawableHitObject h, JudgementResult judgement)
+        {
+            if (judgement.IsHit)
+                hitExplosionContainer.Add(new HitExplosion(h as DrawableHishigataHitObject));
         }
     }
 }
