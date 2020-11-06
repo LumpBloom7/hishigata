45547100d20e79d8485242b2c0df0d995f82e7d1
Derrick Timmermans <derrick.timmermans@outlook.com>
2020-11-06 22:23:31 +0100

    Fix tumor in path arrow

--- osu.Game.Rulesets.Hishigata/UI/Components/PathPlayerArrow.cs
+++ osu.Game.Rulesets.Hishigata/UI/Components/PathPlayerArrow.cs
@@ -18,7 +18,7 @@ namespace osu.Game.Rulesets.Hishigata.UI.Components
             PathRadius = 5;
 
             AutoSizeAxes = Axes.None;
-            Size = new Vector2(( PathRadius + size ) * 2);
+            Size = new Vector2((PathRadius + size) * 2);
 
             angle.BindValueChanged(x => redraw());
             arc.BindValueChanged(x => redraw());
@@ -35,14 +35,14 @@ namespace osu.Game.Rulesets.Hishigata.UI.Components
         };
 
         private const float deg_to_rad = MathF.PI / 180;
-        private BindableFloat arc = new BindableFloat( 180 );
-        private BindableFloat angle = new BindableFloat( 0 );
+        private BindableFloat arc = new BindableFloat(180);
+        private BindableFloat angle = new BindableFloat(0);
 
         public void ChangeRotation(float angle, Easing easing = Easing.None)
         {
             FinishTransforms();
 
-            if ( Math.Abs(angle - this.angle.Value) % 360 == 180 )
+            if (Math.Abs(angle - this.angle.Value) % 360 == 180)
             {
                 this.TransformBindableTo(arc, 360, 25, easing)
                     .Then()
@@ -64,7 +64,7 @@ namespace osu.Game.Rulesets.Hishigata.UI.Components
             for (int i = 0; i < count; i++)
             {
                 float progress = i / (float)(count - 1);
-                var vertexAngle = ( angle.Value - (arc.Value / 2) + (progress * arc.Value) ) * deg_to_rad;
+                var vertexAngle = (angle.Value - (arc.Value / 2) + (progress * arc.Value)) * deg_to_rad;
                 LineSegment ray = new LineSegment(Vector2.Zero, new Vector2(MathF.Sin(vertexAngle) * radius, -MathF.Cos(vertexAngle) * radius));
 
                 foreach (var segment in segments)
