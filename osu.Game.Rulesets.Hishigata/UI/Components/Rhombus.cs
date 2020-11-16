4b4169c2e342a50f3984bd83e28ed89afca9ef0a
Derrick Timmermans <derrick.timmermans@outlook.com>
2020-11-16 23:22:41 +0100

    Add extra glow effect

--- osu.Game.Rulesets.Hishigata/UI/Components/Rhombus.cs
+++ osu.Game.Rulesets.Hishigata/UI/Components/Rhombus.cs
@@ -1,6 +1,8 @@
 using osu.Framework.Audio.Track;
+using osu.Framework.Bindables;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Effects;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Utils;
 using osu.Game.Beatmaps.ControlPoints;
@@ -23,19 +25,41 @@ namespace osu.Game.Rulesets.Hishigata.UI.Components
             Masking = true;
             BorderColour = Color4.Gray;
             BorderThickness = 10;
+            EdgeEffect = new EdgeEffectParameters
+            {
+                Type = EdgeEffectType.Glow,
+                Hollow = true,
+                Radius = 20,
+                Colour = Color4.Gray,
+            };
             Child = new Box
             {
                 Colour = Color4.Black,
                 RelativeSizeAxes = Axes.Both,
                 Alpha = .8f
             };
+
+            kiaiBindable.BindValueChanged(updateGlowState, true);
         }
+
+        private BindableBool kiaiBindable = new BindableBool();
 
         protected override void OnNewBeat(int beatIndex, TimingControlPoint timingPoint, EffectControlPoint effectPoint, ChannelAmplitudes amplitudes)
         {
+            kiaiBindable.Value = effectPoint.KiaiMode;
+
             if (amplitudes.Maximum <= .01f) return;
-            FinishTransforms();
+            FinishTransforms(false, nameof(Size));
             this.ResizeTo(effectPoint.KiaiMode ? 459f : 454.5f, 100).Then().ResizeTo(450, 50);
+        }
+
+        private void updateGlowState(ValueChangedEvent<bool> value)
+        {
+
+            if (value.NewValue)
+                FadeEdgeEffectTo(1, 100);
+            else
+                FadeEdgeEffectTo(0, 200);
         }
     }
 }
