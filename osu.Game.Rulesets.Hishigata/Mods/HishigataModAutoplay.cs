5fd60f4657535e2ffdf2d244d1c901f35de2644b
Derrick Timmermans <derrick.timmermans@outlook.com>
2022-04-27 14:33:29 +0200

    Use CreateReplayData

--- osu.Game.Rulesets.Hishigata/Mods/HishigataModAutoplay.cs
+++ osu.Game.Rulesets.Hishigata/Mods/HishigataModAutoplay.cs
@@ -1,18 +1,13 @@
 ﻿using System.Collections.Generic;
 using osu.Game.Beatmaps;
-using osu.Game.Online.API.Requests.Responses;
 using osu.Game.Rulesets.Hishigata.Replays;
 using osu.Game.Rulesets.Mods;
-using osu.Game.Scoring;
 
 namespace osu.Game.Rulesets.Hishigata.Mods
 {
     public class HishigataModAutoplay : ModAutoplay
     {
-        public override Score CreateReplayScore(IBeatmap beatmap, IReadOnlyList<Mod> mods) => new Score
-        {
-            ScoreInfo = new ScoreInfo { User = new APIUser { Username = "Hishi" } },
-            Replay = new HishigataAutoGenerator(beatmap).Generate(),
-        };
+        public override ModReplayData CreateReplayData(IBeatmap beatmap, IReadOnlyList<Mod> mods)
+            => new ModReplayData(new HishigataAutoGenerator(beatmap).Generate(), new ModCreatedUser { Username = "Hishi" });
     }
 }
